ccdc9da6c85cf25a968210d97f11fcbfae7e65e0
nicopriotto <nico.priotto@gmail.com>
2024-05-31 23:46:11 -0300

    Clear after exception, eliminator minor changes

--- rebuild.sh
+++ rebuild.sh
@@ -3,6 +3,6 @@
 sudo docker start TPE
 sudo docker exec -ti TPE make clean -C/root/Toolchain
 sudo docker exec -ti TPE make clean -C/root/
-sudo docker exec -ti TPE make -C/root/Toolchain
-sudo docker exec -ti TPE make -C/root/
+# sudo docker exec -ti TPE make -C/root/Toolchain
+# sudo docker exec -ti TPE make -C/root/
 sudo docker stop TPE
