54925e7a0193685ff70382be0999926676dfa9a0
MANUEL AHUMADA <maahumada@itba.edu.ar>
2024-06-04 23:00:19 -0300

    Fix time day offset

--- Kernel/clock.c
+++ Kernel/clock.c
@@ -21,24 +21,43 @@ static void num_to_str(uint64_t num, char* buffer){
 	*(buffer+1) = num % 10 + '0';
 }
 
-static uint64_t getHoursUTC3(){
-	uint64_t hour = getHours();
+static uint64_t getHoursUTC3(uint64_t hour){
 	if(hour < TIMEZONE_OFFSET){ 
 		return (HOURS_IN_DAY - TIMEZONE_OFFSET) + hour;   
 	}
 	return hour - TIMEZONE_OFFSET;
 }
 
+static int daysInMonth[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
 void get_current_time_string(char* buffer) {
-    num_to_str(getDay(), buffer);
+    int hours = getHours();
+    int flag = (hours < TIMEZONE_OFFSET) ? 1 : 0;
+    int day = getDay();
+    int month = getMonth();
+    int year = getYear();
+    if(flag) {
+        if(day == 1) {
+            if(month == 1) {
+                year--;
+                month = 12;
+            } else {
+                month--;
+            }
+            day = daysInMonth[month-1];
+        } else {
+            day--;
+        }
+    }
+    num_to_str(day, buffer);
     *(buffer+2) = '/';
-    num_to_str(getMonth(), buffer+3);
+    num_to_str(month, buffer+3);
     *(buffer+5) = '/';
-    num_to_str(getYear(), buffer+6);
+    num_to_str(year, buffer+6);
     *(buffer+8) = ' ';
     *(buffer+9) = '-';
     *(buffer+10) = ' ';
-    num_to_str(getHoursUTC3(), buffer+11);
+    num_to_str(getHoursUTC3(hours), buffer+11);
     *(buffer+13) = ':';
     num_to_str(getMinutes(), buffer+14);
     *(buffer+16) = ':';
